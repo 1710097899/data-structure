03f4c18b66e5df9062204b696974ba78ca672342
1710097899 <1710097899@qq.com>
2022-06-20 12:17:11 +0800

    test

--- SelectionSort/test/SelectionSort_test.go
+++ SelectionSort/test/SelectionSort_test.go
@@ -2,18 +2,14 @@ package test
 
 import (
 	"data_structure/SelectionSort"
-	"fmt"
+	"data_structure/SortingHelper"
 	"testing"
-	"time"
 )
 
 func TestSelectionSort(t *testing.T) {
 	datasize := []int{10000, 100000}
 	for _, v := range datasize {
 		arr := SelectionSort.GenerateOrderedArray(v, v)
-		start := time.Now()
-		SelectionSort.SelectionSort(arr)
-		elapsed := time.Since(start)
-		fmt.Println("SelectionSort", "n=", len(arr), " time of use：", elapsed)
+		SortingHelper.SortTest("SelectionSort", arr, t)
 	}
 }
---
+++ SortingHelper/SortingHelper.go
@@ -0,0 +1,34 @@
+package SortingHelper
+
+import (
+	"data_structure/SelectionSort"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func IsSorted(arr []int) error {
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] > arr[i+1] {
+			fmt.Println(arr[i], "----", arr[i+1])
+			fmt.Println(i, "----", i+1)
+			return errors.New("this sort is error ")
+		}
+
+	}
+	return nil
+}
+
+func SortTest(sortName string, arr []int, t *testing.T) {
+	start := time.Now()
+	if sortName == "SelectionSort" {
+		SelectionSort.SelectionSort(arr)
+	}
+	elapsed := time.Since(start)
+	err := IsSorted(arr)
+	if err != nil {
+		t.Error(sortName, " ", err)
+	}
+	fmt.Println(sortName+" n=", len(arr), " time of use：", elapsed)
+}
