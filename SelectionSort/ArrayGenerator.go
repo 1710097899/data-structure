37df5d92a9e45bad245e1de1568545143cb7cbb8
1710097899 <1710097899@qq.com>
2022-06-20 11:59:59 +0800

    test

--- SelectionSort/ArrayGenerator.go
+++ SelectionSort/ArrayGenerator.go
@@ -10,6 +10,7 @@ func GenerateOrderedArray(n int, bound int) []int {
 	//加入随机数生产数组
 	rand.Seed(time.Now().Unix())
 	for i := 0; i < n; i++ {
+		//生产一个长度为n（i），范围在[0,bound]范围的数组
 		arr = append(arr, rand.Intn(bound))
 	}
 	return arr
