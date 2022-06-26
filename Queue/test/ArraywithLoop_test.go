5043a608dbc9e52eb2ba3063729e4084f46cde43
1710097899 <1710097899@qq.com>
2022-06-26 12:58:35 +0800

    ArraywithLoop

---
+++ Queue/test/ArraywithLoop_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"data_structure/Queue"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestQueue(t *testing.T) {
+	opCount := 100000
+	loopQueue := Queue.NewLoopQueue(10)
+	queue := testQueue(loopQueue, opCount)
+	fmt.Println("loopQueue time  : ", queue)
+	ArrayQueue := Queue.NewArrayQueue(10)
+	queue2 := testQueue(ArrayQueue, opCount)
+	fmt.Println("ArrayQueue time  : ", queue2)
+	LinkedListQueue := Queue.NewLinkedListQueue()
+	queue3 := testQueue(LinkedListQueue, opCount)
+	fmt.Println("LinkedListQueue time  : ", queue3)
+
+}
+
+//测试入队和出队的时间
+func testQueue(queue Queue.Queue, opCount int) time.Duration {
+	start := time.Now()
+	for i := 0; i < opCount; i++ {
+		queue.Enqueue(rand.Intn(100))
+	}
+	for i := 0; i < opCount; i++ {
+		queue.Dequeue()
+	}
+	elapsed := time.Since(start)
+
+	return elapsed
+}
