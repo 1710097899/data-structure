125c47a8aa05b75353733dc1c4d9ae13bc51bd5c
1710097899 <1710097899@qq.com>
2022-06-26 12:57:58 +0800

    LinkedListQueue

---
+++ Queue/test/LinkedListQueue_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"data_structure/Queue"
+	"fmt"
+	"testing"
+)
+
+func TestLinkedListQueue(t *testing.T) {
+	LinkedListQueue := Queue.NewLinkedListQueue()
+	for i := 0; i < 10; i++ {
+		LinkedListQueue.Enqueue(i)
+		fmt.Println(LinkedListQueue)
+		if i%3 == 2 {
+			LinkedListQueue.Dequeue()
+			fmt.Println(LinkedListQueue)
+		}
+	}
+}
